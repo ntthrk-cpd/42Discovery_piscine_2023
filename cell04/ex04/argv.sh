7071edb4a7c518f0b9b931ebff645e7d74103361
ntthrk@h <ntthrk.cpn1994@gmail.com>
2023-08-28 15:30:10 +0700

    completed cell04

--- cell04/ex04/argv.sh
+++ cell04/ex04/argv.sh
@@ -8,13 +8,4 @@ if [ $# -gt 0 ]
     done 
 else 
     echo "No arguments passed"; 
-fi 
-
-###
-#test
-#./argv.sh 1 2 3 4 5 6 7 8 9 10
-# or
-#./argv.sh {1..10}
-# or
-#./argv.sh $(seq 1 10)
-# or
+fi
